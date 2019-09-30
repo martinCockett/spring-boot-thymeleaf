db71e499aeb976e17a7cdaf319abc103b0d3a356
martinCockett <33915187+martinCockett@users.noreply.github.com>
2019-09-30 17:52:55 +0100

    initial commit to remote

---
+++ spring-init-example/src/main/java/com/example/springinitexample/SpringInitExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInitExampleApplication.class, args);
+    }
+
+}
